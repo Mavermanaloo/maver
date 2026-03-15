989f19756d9c88686e3ff87a86b00be580798210
johnbahaynon-go8 <john.bahaynon@go8.tech>
2026-03-15 11:56:59 +0800

    Hosting set ups

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+const repoName = "maver";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: isGitHubPages ? `/${repoName}` : "",
+  assetPrefix: isGitHubPages ? `/${repoName}/` : "",
+};
+
+export default nextConfig;
